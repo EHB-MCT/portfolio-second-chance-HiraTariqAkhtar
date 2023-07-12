558909ff38af6700e14ee1bd10fab321caf11c57
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-07-12 12:39:55 +0200

    Test: remove async from function
    Async functions don't throw errors, they return a promise

--- src/helpers/checkName.js
+++ src/helpers/checkName.js
@@ -4,7 +4,7 @@
  * @param name(string) : enterd name in form
  * @returns (char): first letter of entered name
  */
-async function checkName(name) {
+function checkName(name) {
     let letters = /^[A-Za-z]+$/;
     if (!name.match(letters)) {
         throw new Error("Name should not contain any spaces, numbers or special characters")
