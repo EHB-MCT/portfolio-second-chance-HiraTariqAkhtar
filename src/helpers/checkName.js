3ac75aa53af3fb0a36c6920d8a9cc35144e02660
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2023-07-12 13:05:08 +0200

    Test: Create an if statement per condition

--- src/helpers/checkName.js
+++ src/helpers/checkName.js
@@ -6,11 +6,24 @@
  */
 function checkName(name) {
     let letters = /^[A-Za-z]+$/;
-    if (!name.match(letters)) {
-        throw new Error("Name should not contain any spaces, numbers or special characters")
-      }
-    const firstLetter = name.charAt(0).toLowerCase()
-    return firstLetter
+    let numbers = /^[0-9a-zA-Z]+$/;
+    let space = /\s/;
+    let specialCharacters = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+
+    if (name.match(letters)) {
+        const firstLetter = name.charAt(0).toLowerCase()
+        return firstLetter
+    } 
+    if(name.match(numbers)) {
+        throw new Error("Name should not contain numbers")
+    }
+    if(name.match(space)) {
+        throw new Error("Name should not contain spaces")
+    }
+    if(name.match(specialCharacters)) {
+        throw new Error("Name should not contain special characters")
+    }
+    throw new Error("Name should not contain any spaces, numbers or special characters")
 }
 
 module.exports = {
